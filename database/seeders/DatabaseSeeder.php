fa252fbe77c5e19928cf4288f26d0de4a5ccf116
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-24 13:55:53 +0100

    [add] TechnologyTbaleSeeder in databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProjectSeeder::class,
             TypeTableSeeder::class,
             ProjectTypeSeeder::class,
+            TechnologyTableSeeder::class,
         ]);
     }
 }
