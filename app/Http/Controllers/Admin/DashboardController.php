ed7743fe10d89d32f677af37d35a0cbd08595f83
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-01-24 17:07:19 +0100

    [change] dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use App\Models\Type;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,6 +13,10 @@ class DashboardController extends Controller
 {
     public function index(){
         $user = Auth::user();
-        return view('admin.home');
+        $projects = Project::all();
+        $types = Type::all();
+        $technologies = Technology::all();
+
+        return view('admin.home',compact('projects','types','technologies'));
     }
 }
